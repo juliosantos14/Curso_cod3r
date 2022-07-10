ac73e2844b45eecc1df6483e8b3be9b3288f53e2
juliosantos14 <julio.cordeiro14@gmail.com>
2022-07-10 01:08:43 -0300

    .

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Fundamentos/OperadoresUnarios.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Fundamentos/OperadoresUnarios.cs
@@ -10,7 +10,32 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
-             
+            var valorNegativo = -5;
+            var numero1 = 2;
+            var numero2 = 3;
+            var boolean = true;
+
+            Console.WriteLine(+valorNegativo);
+            Console.WriteLine(-valorNegativo);
+            Console.WriteLine(valorNegativo);
+
+            Console.WriteLine(!boolean);
+
+            numero1++;
+            Console.WriteLine(numero1);
+
+            --numero1;
+            Console.WriteLine(numero1);
+            numero1--;
+            Console.WriteLine(numero1);
+
+            numero1++;
+            Console.WriteLine(numero1);
+
+            Console.WriteLine(++numero1 == --numero2);//Valor false
+            Console.WriteLine(numero1++ == --numero2);//Valor verdadeiro pois altera o valor de numero 2 antes por estar com -- antes da variavel
+            Console.WriteLine($"{numero1} {numero2}");
+
         }
     }
 }
