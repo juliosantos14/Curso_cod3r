1dea63f8f27dd41f07c15b24a4d6620f3e2a649c
juliosantos14 <julio.cordeiro14@gmail.com>
2022-07-27 00:15:18 -0300

    Parametros nomeados

---
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/MetodosEFuncoes/ParametrosNomeados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.MetodosEFuncoes
+{
+    internal class ParametrosNomeados
+    {
+        public static void Formatar(int dia, int mes, int ano)
+        {
+            Console.WriteLine("{0:D2}/{1:D2}/{2}", dia, mes, ano);
+        }
+        public static void Executar()
+        {
+            Formatar(mes: 10,dia:25,ano:2025);
+        }
+    }
+}
--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Program.cs
@@ -37,6 +37,9 @@ namespace CursoCSharp
                 {"Metodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar},
                 {"Atributos Estaticos - Classes e Métodos", AtributosEstaticos.Executar},
                 {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
+                {"Params - Classes e Métodos", Params.Executar},
+                {"Parametros nomeados - Classes e Métodos", ParametrosNomeados.Executar},
+
             });
 
             central.SelecionarEExecutar();
