d247223a95ae9d5d955a0b1894c97ee2fd0e1294
juliosantos14 <julio.cordeiro14@gmail.com>
2022-08-16 00:06:52 -0300

    .

---
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Colecoes/ColecoesList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes
+{
+    internal class ColecoesList
+    {
+        public class Produto
+        {
+            public string Nome;
+            public double Preco;
+            public Produto(string nome, double preco)
+            {
+                Nome = nome;
+                Preco = preco;
+            }
+        }
+        public static void Executar()
+        {
+
+        }
+    }
+}
--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Program.cs
@@ -52,6 +52,7 @@ namespace CursoCSharp
 
                 //Coleções
                 {"Array - Coleções", Colecoes.Array.Executar},
+                {"List - Coleções", ColecoesList.Executar},
 
             });
 
