ae4c3028ddb4f46d83bb78c5797134674a2ba008
juliosantos14 <julio.cordeiro14@gmail.com>
2022-08-23 16:31:23 -0300

    Estudos List

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Colecoes/ColecoesList.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Colecoes/ColecoesList.cs
@@ -20,7 +20,18 @@ namespace CursoCSharp.Colecoes
         }
         public static void Executar()
         {
+            var Livroi = new Produto("Game of Thrones", 149.99);
+            var Carrinho = new List<Produto>();
 
+            var combo = new List<Produto>
+            {
+                new Produto("Camisa", 29.9),
+                new Produto("Temporada Lucifer", 99.9),
+                new Produto("Poster", 10)
+            };
+
+            Carrinho.AddRange(combo);
+            Console.WriteLine(Carrinho.Count);
         }
     }
 }
