4fc685eb4ad9ef9fb75fb3dcb815434599b0a8b4
juliosantos14 <julio.cordeiro14@gmail.com>
2022-08-29 00:24:54 -0300

    .

---
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/Colecoes/ColecoesQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Colecoes
+{
+    internal class ColecoesQueue
+    {
+        public static void Executar()
+        {
+            var fila = new Queue<string>();
+            fila.Enqueue("Adriana");
+            fila.Enqueue("Anderson");
+            fila.Enqueue("Abilene");
+            fila.Enqueue("Clayton");
+
+            Console.WriteLine(fila.Peek());
+        }
+    }
+}
