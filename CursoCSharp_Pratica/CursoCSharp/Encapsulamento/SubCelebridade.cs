61b254385fd431c7351256e788ce99180a5e1819
juliosantos14 <julio.cordeiro14@gmail.com>
2022-11-22 23:59:56 -0300

    .

---
+++ CursoCSharp_Pratica/CursoCSharp/Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class SubCelebridade
+    {
+        //Todos 
+        public string InfoPublica = "Tenho um instagram";
+
+        //Herança
+        protected string CorDoOlho = "Verde";
+
+        //Mesmo projeto (Assembly)
+        internal ulong NumeroCelular = 223333333333333333;
+    }
+}
