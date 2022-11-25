9c295ac6cb7fd61bfd30be6c2b4480efce074e87
juliosantos14 <julio.cordeiro14@gmail.com>
2022-11-25 14:17:55 -0300

    ..

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Encapsulamento.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Encapsulamento.cs
@@ -11,14 +11,46 @@ namespace CursoCSharp.OO
     {
         public new void MeusAcessos()
         {
+            Console.WriteLine();
+            Console.WriteLine("Filho não reconhecido ...");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            //Console.WriteLine(NumeroDeCelular);
+            Console.WriteLine(JeitoDeFalar);
+            //Console.WriteLine(SegredoFamilia);
+            //Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
 
+    public class AmigoDistante
+    {
+        public readonly SubCelebridade amiga = new SubCelebridade();
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine();
+            Console.WriteLine("AmigoDistante...");
+
+            Console.WriteLine(amiga.InfoPublica);
+            //Console.WriteLine(amiga.CorDoOlho);
+            //Console.WriteLine(amiga.NumeroCelular);
+            //Console.WriteLine(amiga.JeitoDeFalar);
+            //Console.WriteLine(amiga.SegredoFamilia);
+            //Console.WriteLine(amiga.UsaMuitoPhotoshop);
         }
     }
     internal class Encapsulamento
     {
         public static void Executar()
         {
+            SubCelebridade sub = new SubCelebridade();
+            sub.MeusAcessos();
 
+            new FilhoReconhecido().MeusAcessos();
+            new AmigoProximo().MeusAcessos();
+
+            new FilhoNaoReconhecido().MeusAcessos();
+            new AmigoDistante().MeusAcessos();
         }
     }
 }
