4c6561a178a1cf0b3d55607dd57b7995c4f6e0e0
juliosantos14 <julio.cordeiro14@gmail.com>
2022-11-28 10:22:18 -0300

    ..

---
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Polimorfismo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.OO
+{
+    public class Feijao
+    {
+
+    }
+    public class Arroz
+    {
+
+    }
+    public class Carne
+    {
+
+    }
+    class Polimorfismo
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
