ba753ebe13ddefa555ce3cef283bf2e1947ae213
juliosantos14 <julio.cordeiro14@gmail.com>
2022-11-30 00:01:40 -0300

    ..

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Polimorfismo.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Polimorfismo.cs
@@ -8,21 +8,54 @@ namespace CursoCSharp.OO
 {
     public class Feijao
     {
-
+        public double Peso;
     }
     public class Arroz
     {
-
+        public double Peso;
     }
     public class Carne
     {
+        public double Peso;
+    }
 
+    public class Pessoa
+    {
+        public double Peso;
+
+        public void Comer(Feijao feijao)
+        {
+            Peso+=feijao.Peso;
+        } 
+        public void Comer(Arroz arroz)
+        {
+            Peso+=arroz.Peso;
+        } 
+        public void Comer(Carne carne)
+        {
+            Peso+=carne.Peso;
+        }
     }
     class Polimorfismo
     {
         public static void Executar()
         {
+            Feijao ingrediente1 = new Feijao();
+            ingrediente1.Peso = 0.3;
 
+            Arroz ingrediente2 = new Arroz();
+            ingrediente2.Peso = 0.25;
+
+            Carne ingrediente3 = new Carne();
+            ingrediente3.Peso = 0.3;
+
+            Pessoa cliente1= new Pessoa();
+            cliente1.Peso = 80.2;
+            cliente1.Comer(ingrediente1);
+            cliente1.Comer(ingrediente2);
+            cliente1.Comer(ingrediente3);
+
+            Console.WriteLine($"Agora o peso do cliente é {0}", cliente1.Peso);
         }
     }
 }
