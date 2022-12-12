6982724b815da86fca9ec25aca49257600b22296
juliosantos14 <julio.cordeiro14@gmail.com>
2022-12-12 12:21:33 -0300

    Interface

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Interface.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/OO/Interface.cs
@@ -41,12 +41,31 @@ namespace CursoCSharp.OO
             new Subtracao(),
             new Multiplicacao()
         };
+
+        public string ExecutarOperacoes(int a, int b)
+        {
+            string resultado = "";
+
+            foreach (var op in operacoes)
+            {
+                resultado += $"Usando {op.GetType().Name} = {op.Operacao(a, b)}\n";
+            }
+
+            return resultado;
+        }
     }
     internal class Interface
     {
         public static void Executar()
         {
-
+            var calc = new Calculadora();
+            Console.WriteLine("Digite o primeiro numero");
+            var v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDigite o segundo numero");            
+            var v2 = int.Parse(Console.ReadLine());
+            //var resultado = calc.ExecutarOperacoes(20, 5);
+            var resultado = calc.ExecutarOperacoes(v1, v2);
+            Console.WriteLine(resultado);
         }
     }
 }
