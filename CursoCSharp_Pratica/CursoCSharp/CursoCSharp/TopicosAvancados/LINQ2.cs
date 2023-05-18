d02547fd5816287e3fc7b09dc6c127bcf4f7c7ec
juliosantos14 <julio.cordeiro14@gmail.com>
2023-05-18 15:11:39 -0300

    ..

--- CursoCSharp_Pratica/CursoCSharp/CursoCSharp/TopicosAvancados/LINQ2.cs
+++ CursoCSharp_Pratica/CursoCSharp/CursoCSharp/TopicosAvancados/LINQ2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,25 +11,28 @@ namespace CursoCSharp.TopicosAvancados
     {
         public static void Executar()
         {
+
             var alunos = new List<Aluno>
             {
-                new Aluno(){Nome = "Pedro", Idade = 24, Nota=8.0},
-                new Aluno(){Nome = "Andre", Idade = 21, Nota=4.3},
-                new Aluno(){Nome = "Alessandro", Idade = 24, Nota=7.5},
-                new Aluno(){Nome = "Wellington", Idade = 25, Nota=9.2},
-                new Aluno(){Nome = "Davi", Idade = 24, Nota=8.4},
-                new Aluno(){Nome = "Julia", Idade = 24, Nota=3.7},
-                new Aluno(){Nome = "Ana", Idade = 24, Nota=2.1},
-            };
+                    new Aluno(){Nome = "Pedro", Idade = 24, Nota=8.0},
+                    new Aluno(){Nome = "Andre", Idade = 21, Nota=4.3},
+                    new Aluno(){Nome = "Alessandro", Idade = 24, Nota=7.5},
+                    new Aluno(){Nome = "Wellington", Idade = 25, Nota=9.2},
+                    new Aluno(){Nome = "Davi", Idade = 24, Nota=8.4},
+                    new Aluno(){Nome = "Julia", Idade = 24, Nota=3.7},
+                    new Aluno(){Nome = "Ana", Idade = 24, Nota=2.1},
+                };
 
             var pedro = alunos.Single(a => a.Nome.Equals("Pedro"));
             Console.WriteLine($"{pedro.Nome}, {pedro.Nota}");
 
             var fulano = alunos.SingleOrDefault(a => a.Nome.Equals("Fulano"));
-            if(fulano == null)
+            if (fulano == null)
             {
                 Console.WriteLine("Aluno Inexistente");
-            }
-        }
+
+
+            };
     }
+}
 }
